a07ab46bc1d2cb628a77f384f5d2cbac5b46c2cf
tnarik <tnarik@gmail.com>
2014-08-13 12:35:42 +0100

    Added simplecov/coveralls to the test helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 require 'bundler/setup'
 Bundler.setup
 
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  Coveralls::SimpleCov::Formatter,
+  SimpleCov::Formatter::HTMLFormatter
+]
+SimpleCov.start
+
 require 'hostsfile'
